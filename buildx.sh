2b48e767a552d003af409e5cc654a4ecb3e501c9
Mike Nye <mike.nye@gmail.com>
2020-08-20 11:25:59 +0800

    Linting

--- buildx.sh
+++ buildx.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 VERSION=latest
 IMAGE=mikenye/youtube-dl
@@ -18,16 +18,16 @@ until [ $n -ge 5 ]
 do
     build_version=$(docker run --rm mikenye/youtube-dl:latest --version || exit 1)
     if [ $? -eq 0 ]; then
-        build_version=$(echo $build_version | sed 's/\r$//')
+        build_version=$(echo "$build_version" | sed 's/\r$//')
         break
     fi
-    n=$[$n+1]
+    n=$((n+1))
     sleep 15
 done
-if [ $n -ge 5 ]; then
+if [ "$n" -ge 5 ]; then
     echo "Failed when trying to get youtube-dl version in latest container :("
     exit 1
 fi
 
 # If the build was successful, then we can tag with current version
-docker buildx build -t ${IMAGE}:${build_version} --progress=plain --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 . || exit 1
+docker buildx build -t "${IMAGE}:${build_version}" --progress=plain --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 . || exit 1
